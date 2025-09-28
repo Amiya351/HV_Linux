fe36f1d41f056f5c31c0e1b1f7fe5776d95dce0b
Amiya351 <amiyadas351@gmail.com>
2025-09-29 01:57:16 +0530

    This is the second comit for shell script

---
+++ lab3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#author: Amiya
+#description: this program explains doing basic mathematics using shel script
+
+echo "Enter the first number"
+
+read a
+
+echo "Enter the second number"
+
+read b
+
+result=$((a+b))
+
+echo $result 
